61e499489b0d968232783abece611e72c91d4399
Muhammad Moeen <muhammad.moeen@themoonshotfactory.com>
2025-06-05 13:53:46 +0500

    added stack

---
+++ app/data_structures/stack/stack.py
@@ -0,0 +1,52 @@
+from typing import Generator
+
+
+class Stack[T]:
+    capacity: int
+    index: int
+    data: list[T | None]
+
+    def __init__(self, capacity=5) -> None:
+        self.capacity = capacity
+        self.index = 0
+        self.data = [None] * capacity
+
+    def size(self) -> int:
+        return self.index
+
+    def values(self) -> Generator[T]:
+        for i in range(self.index):
+            value = self.data[i]
+            if value is not None:
+                yield value
+
+    def push(self, value: T) -> None:
+        if self.index < self.capacity:
+            self.data[self.index] = value
+            self.index += 1
+            return
+
+        # resize operation.
+        new_capacity = self.capacity * 2
+        new_data: list[T | None] = [None] * new_capacity
+        for i in range(self.index):
+            new_data[i] = self.data[i]
+
+        new_data[self.index] = value
+        self.data = new_data
+        self.capacity = new_capacity
+        self.index += 1
+
+    def pop(self) -> T | None:
+        if self.index == 0:
+            return None
+
+        value = self.data[self.index - 1]
+        self.index -= 1
+        return value
+
+    def peek(self) -> T | None:
+        if self.index == 0:
+            return None
+
+        return self.data[self.index - 1]
---
+++ app/data_structures/stack/stack_test.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from .stack import Stack
+
+
+class TestStack(TestCase):
+    def test_normal_usage(self) -> None:
+        s = Stack[int](capacity=5)
+        self.assertEqual(s.capacity, 5)
+        for i in range(4):
+            s.push(i + 1)
+
+        self.assertEqual(s.size(), 4)
+        self.assertEqual(s.capacity, 5)
+
+        s.push(5)
+        self.assertEqual(5, s.size())
+        s.push(6)
+        self.assertEqual(6, s.size())
+        self.assertEqual(10, s.capacity)
+
+        self.assertEqual(s.peek(), 6)
+        self.assertEqual(s.size(), 6)
+        self.assertEqual(s.pop(), 6)
+        self.assertEqual(s.size(), 5)
+
+    def test_empty_stack(self) -> None:
+        s = Stack[int](10)
+        self.assertEqual(s.size(), 0)
+        self.assertEqual(s.capacity, 10)
+
+        values = list(s.values())
+        self.assertEqual(values, [])
+        self.assertIsNone(s.pop())
+        self.assertIsNone(s.peek())
