1b13d13e1c1eff003f3589fc2aa6857320fbab49
moeenn <moeen.v8@gmail.com>
2025-06-09 02:09:07 +0500

    solved three sum

--- app/questions/arrays/three_sum_test.py
+++ app/questions/arrays/three_sum_test.py
@@ -14,11 +14,13 @@ class TestThreeSum(TestCase):
         Scenario(
             nums=[-1, 0, 1, 2, -1, -4], expected=[Triplet(-1, -1, 2), Triplet(-1, 0, 1)]
         ),
+        Scenario(nums=[0, 1, 1], expected=[]),
+        Scenario(nums=[0, 0, 0], expected=[Triplet(0, 0, 0)]),
     ]
 
     def test_usage(self) -> None:
         for s in self.scenarios:
             got = three_sum(s.nums)
             self.assertEqual(len(got), len(s.expected))
-            # TODO: fixme.
-            self.assertEqual(got, s.expected)
+            for triplet in got:
+                self.assertTrue(triplet.is_included(s.expected))
