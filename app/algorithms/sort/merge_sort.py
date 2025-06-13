23196d2ab10a060119f5ff2b340253254e91b000
Muhammad Moeen <muhammad.moeen@themoonshotfactory.com>
2025-06-13 18:55:11 +0500

    merge sort +progress

--- app/algorithms/sort/merge_sort.py
+++ app/algorithms/sort/merge_sort.py
@@ -1,2 +1,34 @@
-def merge_sort(data: list[int]) -> list[int]:
-    raise Exception("not implemented")
+def merge(a: list[int], b: list[int]) -> list[int]:
+    result: list[int] = []
+    i = 0
+    j = 0
+
+    while i != len(a) and j != len(b):
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+
+        if i == len(a):
+            result.append(b[j])
+
+        if j == len(b):
+            result.append(a[i])
+
+    return result
+
+
+def merge_sort(nums: list[int]) -> list[int]:
+    size = len(nums)
+
+    chunks: list[list[int]] = []
+    for n in nums:
+        chunks.append([n])
+
+    result = chunks[0]
+    for i in range(size - 1):
+        result = merge(result, [i])
+
+    return result
